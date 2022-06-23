f8d0d9d13fdd489ff40559a7aa4dab9dfa404b79
Marcos Henrique <marcosmarcone48@gmail.com>
2022-06-23 13:56:29 -0300

    update deploy link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ffm-linkr.herokuapp.com",
+  //baseURL: "http://localhost:5000",
 });
 
 export const signUp = async (formData) => {
